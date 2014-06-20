eb38657e16a3b8bae79e573349df6ca1bfc03e92
Matt Tse <matt@firebase.com>
2014-06-20 15:41:07 -0700

    more features'

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -4,11 +4,6 @@ var fbBaseURL = 'https://blistering-fire-3878.firebaseio.com/';
 var gamesListRef = new Firebase('https://blistering-fire-3878.firebaseio.com/games');
 
 
-function targetCellSelector(row, col){
-  var rowID = 'r' + row;
-  var colID = 'c' + col;
-  return '#' + rowID + ' ' + '#' + colID;
-}
 
 function renderMove(moveObj) {
   var $targetCell = $(targetCellSelector(moveObj.row, moveObj.col));
@@ -23,11 +18,26 @@ function renderMove(moveObj) {
     $targetCell.addClass('black-move');
   }
   $targetCell.addClass('occupied');
+
+  // Check for killed stones/groups
+  var dx = [1, -1, 0, 0];
+  var dy = [0, 0, 1, -1];
+  // for(i = 0; i < 4; i++){
+
+  // }
 }
 
-function isValidMove(row, col) {
+function targetCellSelector(row, col){
+  var rowID = 'r' + row;
+  var colID = 'c' + col;
+  return '#' + rowID + ' ' + '#' + colID;
+}
+function isValidMove(row, col, moveCount) {
+  if ($('#board').hasClass('black-to-play') && !$('#board').hasClass('black-player')) {return false;}
+  if ($('#board').hasClass('white-to-play') && !$('#board').hasClass('white-player')) {return false;}
   var $targetCell = $(targetCellSelector(row, col));
-  return !$targetCell.hasClass('occupied');
+  if ($targetCell.hasClass('occupied')) {return false;}
+  return true;
 }
 
 function updateMoveCounterDisplay(moveCount) {
@@ -42,19 +52,24 @@ function updateMoveCounterDisplay(moveCount) {
 }
 
 function setBlackPlay() {
-  $('#board').addClass('black-play');
-  $('#board').removeClass('white-play');
+  $('#board').addClass('black-to-play');
+  $('#board').removeClass('white-to-play');
 }
 function setWhitePlay() {
-  $('#board').addClass('white-play');
-  $('#board').removeClass('black-play');
+  $('#board').addClass('white-to-play');
+  $('#board').removeClass('black-to-play');
 }
 
 
-var loadGame = function(event) {
+function loadGame(event) {
   // Refresh the game data upon open
   var gameRef = new Firebase(fbBaseURL + '/games/' + event.data.gameID);
 
+
+  var myName = "Anonymous User";
+  var spectatorsRef = gameRef.child('spectators');
+  var mySpectatorEntryRef = spectatorsRef.push({name: myName});
+  mySpectatorEntryRef.onDisconnect().remove();
 
   // Load up the game
   gameRef.once('value', function(gameSnapshot) {
@@ -64,8 +79,9 @@ var loadGame = function(event) {
     var clickCell = function (clickEvent) {
       var row = clickEvent.data.row;
       var col = clickEvent.data.col;
+      var moveCount = clickEvent.data.moveCount;
       // Verify that the move is valid.
-      if (isValidMove(row, col)) {
+      if (isValidMove(row, col, moveCount)) {
         var moveCountRef = new Firebase(fbBaseURL + '/games/' + event.data.gameID + '/moveCount');
         moveCountRef.once('value', function(moveCountSnapshot) {
           var moveCount = moveCountSnapshot.val();
@@ -89,12 +105,62 @@ var loadGame = function(event) {
 
       $('#board').show();
       $('#board').empty();
+      $('#spectators').show();
       setBlackPlay();
       $('#gameTitle').show();
       $('#gameTitle').text(refreshedGame.name);
       updateMoveCounterDisplay(refreshedGame.moveCount);
 
-      //Render the board grid
+      // Register Spectator Button Events
+      $('#playAsBlack').click({}, playAsBlack);
+      $('#playAsWhite').click({}, playAsWhite);
+      var blackPlayerRef = gameRef.child('blackPlayer');
+      var whitePlayerRef = gameRef.child('whitePlayer');
+      var mySpectatorID = mySpectatorEntryRef.name();
+      blackPlayerRef.on('value', function(snapshot){
+        if (snapshot.val() !== null) {
+          $('#playAsBlack').addClass('disabled');
+        } else {
+          $('#playAsBlack').removeClass('disabled');
+        }
+      });
+      whitePlayerRef.on('value', function(snapshot){
+        if (snapshot.val() !== null) {
+          $('#playAsWhite').addClass('disabled');
+        } else {
+          $('#playAsWhite').removeClass('disabled');
+        }
+      });
+      function playAsBlack () {
+        blackPlayerRef.set({name: myName, id: mySpectatorID});
+        $('#board').addClass('black-player');
+        blackPlayerRef.onDisconnect().remove();
+      }
+      function playAsWhite () {
+        whitePlayerRef.set({name: myName, id: mySpectatorID});
+        $('#board').addClass('white-player');
+        whitePlayerRef.onDisconnect().remove();
+      }
+
+
+      // Render board grid numbers
+      $topRuler = $("<div class='board-ruler ruler-top'></div>");
+      $topRuler.appendTo($('#board'));
+      for (r = 1; r <= refreshedGame.size; r++){
+        var markerHTML = Mustache.render("<div class='ruler-marker'>{{marker}}</div>", {marker: r});
+        $marker = $(markerHTML);
+        $marker.appendTo($topRuler);
+      }
+
+      $leftRuler = $("<div class='board-ruler ruler-vertical'></div>");
+      $leftRuler.appendTo($('#board'));
+      for (r = 1; r <= refreshedGame.size; r++){
+        var markerHTML = Mustache.render("<div class='ruler-marker'>{{marker}}</div>", {marker: r});
+        $marker = $(markerHTML);
+        $marker.appendTo($leftRuler);
+      }
+
+      // Render the board grid
       for (r = 0; r < refreshedGame.size - 1; r++) {
         $gridRow = $("<div class='grid-row'></div>");
         $gridRow.appendTo($('#board'));
@@ -105,21 +171,21 @@ var loadGame = function(event) {
       }
 
       // Render the board stones
-      for (r = 0; r < refreshedGame.size; r++) {
+      for (r = 1; r <= refreshedGame.size; r++) {
         var row = {
           rowID: 'r' + r
         };
         var rowHTML = Mustache.render("<div class='stone-row' id='{{rowID}}'></div>", row);
         var $row = $(rowHTML);
         $row.appendTo($('#board'));
-        for (c = 0; c < refreshedGame.size; c++) {
+        for (c = 1; c <= refreshedGame.size; c++) {
           var col = { colID: 'c' + c};
           var cellHTML = Mustache.render("<div class='stone-col' id={{colID}}></div>", col);
           var $cell = $(cellHTML);
           $cell.appendTo($row);
 
           // Click callback
-          $cell.click({row: r, col: c}, clickCell);
+          $cell.click({row: r, col: c, moveCount: refreshedGame.moveCount}, clickCell);
         }
       }
 
@@ -136,8 +202,23 @@ var loadGame = function(event) {
       moveCountRef.on('value', function(snapshot) {
         updateMoveCounterDisplay(snapshot.val());
       });
+
+      // Callback for new game spectators
+      spectatorsRef.on('child_added', function(snapshot){
+        spectatorObj = _.extend(snapshot.val(), {id: snapshot.name()});
+        var spectatorHTML = Mustache.render("<li class='spectator-name' data-id='{{id}}'>{{name}}</li>", spectatorObj);
+        $('#spectators .spectator-list').append($(spectatorHTML));
+      });
+
+      // Callback for spectators leaving
+      spectatorsRef.on('child_removed', function(snapshot){
+        var spectatorID = snapshot.name();
+        var jquerySelector = Mustache.render(".spectator-name[data-id='{{id}}']", {id: spectatorID});
+        $(jquerySelector).remove();
+      });
+
   });
-};
+}
 
 function listNameLabel (game) {
   return Mustache.render("{{name}} | {{size}}x{{size}} | Move {{moveCount}}", game);
