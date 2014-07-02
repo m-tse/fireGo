0e70a36b9082d7bb29f7eb74050febf1cc67d52f
Matthew Tse <matt@firebase.com>
2014-07-02 15:21:51 -0700

    progress

--- public/assets/js/models.js
+++ public/assets/js/models.js
@@ -1,7 +1,20 @@
 // Includes all Backbone Models
 
 var GameModel = Backbone.Model.extend({
-
+  initialize: function() {
+    this.moveOccurred();
+    this.on('change:moves', this.moveOccurred);
+  },
+  moveOccurred: function() {
+    this.set('totalMoves', this.totalMoves);
+    this.set('currentTurnColor', this.currentTurnColor);
+  },
+  totalMoves: function() {
+    return _.keys(this.moves).length + 1;
+  },
+  currentTurnColor: function() {
+    if (this.totalMoves() % 2 === 0) {return 'black';} else {return 'white';}
+  }
 });
 
 var GamesModel = Backbone.Firebase.Collection.extend({
--- public/assets/js/views.js
+++ public/assets/js/views.js
@@ -2,6 +2,10 @@
 
 var GamesLobbyView = Backbone.View.extend({
   el: $('#gameLobbyBackbone'),
+  template: Templates['public/templates/gameLobby.hbs'],
+  events: {
+    "click .js-create-game": "createGame"
+  },
 
   initialize: function() {
     this.listenTo(this.collection, "add remove", this.render);
@@ -9,35 +13,59 @@ var GamesLobbyView = Backbone.View.extend({
 
   render: function() {
     this.$el.empty();
+    this.$el.html(this.template());
     for (var i = 0; i < this.collection.length; i++) {
       var gameModel = this.collection.models[i];
       var gameLabelView = new GameLabelView({model:gameModel});
-
-      this.$el.append(gameLabelView.render().el);
+      $('.js-games-list').append(gameLabelView.render().el);
     }
     this.$el.append(Templates['public/templates/testTemplate.hbs']);
-    console.log(Templates['public/templates/testTemplate.hbs']);
-    // window.v = JST['templates/post/detail.hbs'];
     return this;
+  },
+
+  createGame: function() {
+    var gameInfo = {
+      name: $('.js-game-name').val(),
+      size: 9,
+      moveCount: 1
+    };
+    var newGame = new GameModel(gameInfo);
+    this.collection.add(newGame);
   }
 });
 
 var GameLabelView = Backbone.View.extend({
   tagName: "a",
-  className: "list-group-item",
+  className: "list-group-item games-list-item",
   template: Templates['public/templates/gameLabelTitle.hbs'],
-
+  events: {
+    "click": "openGame"
+  },
   render: function() {
     this.$el.html(this.template(this.model.attributes));
     return this;
+  },
+  openGame: function() {
+    var openedGame = new OpenGameView({model: this.model});
+    $('#gameLobbyBackbone').hide();
+    $('#openGameBackbone').empty().html(openedGame.render().el);
   }
 });
 
 var OpenGameView = Backbone.View.extend({
+  events: {
+    "click .js-return-to-lobby": "returnToLobby"
+  },
+  template: Templates['public/templates/openGame.hbs'],
   initialize: function() {
-
+    this.listenTo(this.model, 'change', this.render);
   },
   render: function() {
-
+    this.$el.html(this.template(this.model.attributes));
+    return this;
+  },
+  returnToLobby: function() {
+    $('#gameLobbyBackbone').show();
+    this.remove();
   }
 });
